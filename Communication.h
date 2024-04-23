4469e32efae1a7de2372d8c4a1839081e381bcc7
Mahdi-toumi <152063136+Mahdi-toumi@users.noreply.github.com>
2024-04-23 21:40:52 +0100

    Update Communication.h

--- Communication.h
+++ Communication.h
@@ -1,91 +1,56 @@
-#include<C:\Users\toumi\Desktop\projet c++\Gestion d'un Hachathon\Message.h>
+#ifndef message_H_INCLUDED
+#define message_H_INCLUDED
+
+#include <C:\Users\toumi\Desktop\projet c++\Gestion d'un Hachathon\Hackathon.h>
+#include <C:\Users\toumi\Desktop\projet c++\Gestion d'un Hachathon\Date.h>
+#include <iostream>
+#include <string>
+#include <vector>
 
 
-int message::nextId = 1;
 
-message::message():id(nextId++) {};
+using namespace std;
 
-message::message( string contenu = "" ):id(nextId++){
-    this->contenu=contenu ;
-}
+class message {
+    static int nextId ;
+
+    int id;
+    organisateur* auteur;
+    string contenu;
+    int destinataires;
+    Date Date_envoi;
+
+public:
+    message() ; /// constructeur de la classe communication
+
+    message( string contenu ) ;
 
 
-istream& operator >>(istream& entree,message& c){
-        cout<<" Saisir la date d'envoie de ce message"<<endl;
-        cin>>c.Date_envoi;
 
-        cout<<" Saisir le message"<<endl;
-        cin>>c.contenu;
 
-        return entree ;
+    ~message(){} /// destructeur
+
+    friend istream& operator >>(istream& entree,message& c) ;
+
+    friend ostream& operator <<(ostream& sortie,message& c);
+
+    int getdestinataires() { return destinataires ;}
+
+
+    void envoyer_message (Hackathon  , vector<message*> ) ; /// Envoi d'un message a un organisateur lors d'un hackathon
+
+    void afficher_messages (   organisateur* o, vector<message*> Message   ); /// Affichage des messages destines a un organisateur
+
+
+
+
+    void creerFichier(const string& ) ; /// Ouverture du fichier
+
+    void remplirDansFichier(const string&) ;  /// remplissage du fichier
+
+    void lireFichier(const string&); /// affichage du fichier
+
 
 };
 
-ostream& operator <<(ostream& sortie,message& c){
-        sortie<<"Message envoyee dans la date : " << c.Date_envoi <<endl ;
-        sortie<<"Contenue : "<< c.contenu<<endl;
-        return sortie ;
-    };
-
-
-
-
-void message::envoyer_message (Hackathon h , vector<message*> Message ) {
-            string nom , prenom ;
-            cout << "Saisir le nom du destinataire : " ; cin >> nom ;
-            cout << "Saisir le prenom du destinataire : " ; cin >> prenom  ;
-            destinataires = h.rechercher_organisateur(nom,prenom)->getid() ; /// Obtient l'identifiant du destinataire a partir du hackathon
-            Message.push_back(this) ; /// Ajoute le message à la fin du vecteur de messages
-        }
-
-void message::afficher_messages (  organisateur* o, vector<message*> Message ){
-            for (unsigned i=0 ; i<Message.size() ; i++) { /// Parcours de tous les messages dans le vecteur
-                if (o->getid()== Message[i]->destinataires  ){ /// Verifie si l'ID de l'organisateur correspond à l'ID du destinataire du message
-                    cout<<this<<endl ; /// Affiche le message actuel
-                    return ;
-                }
-            }
-            cout<<"Pas de message pour le moment ."<<endl ;
-        }
-
-
-
-
-
-void message::creerFichier(const string& nomFichier) {
-            ifstream fichier(nomFichier, ofstream::out | ofstream::trunc);/// Ouverture du fichier en mode ecriture ( effacement du contenu existant)
-
-            if (!fichier.is_open()) {
-                cerr << "Erreur lors de l'ouverture du fichier." << endl;
-                return;
-                }
-
-            cout << "Fichier ouvert avec succes." << endl;
-            fichier.close();
-        }
-
-void message::remplirDansFichier(const string&  nomFichier ) {
-                ofstream fichier(nomFichier, ios::app);      /// Ouverture du fichier en mode append pou l'ajout des donnes
-                if (!fichier.is_open()) {    /// Verification si le fichier est ouvert
-                    cout << "Erreur lors de l'ouverture du fichier en ecriture." << endl;
-                    return;
-                }
-                fichier << *this;   /// Ecriture des informations du participant dans le fichier
-                fichier.close();    /// Fermeture du fichier
-            };
-
-void message::lireFichier(const string& nomFichier ){
-                ifstream fichier(nomFichier);
-                if (!fichier.is_open()) {
-                    cout << "Erreur lors de l'ouverture du fichier en lecture." << endl;
-                    return;
-                }
-                string ligne;
-                while (getline(fichier, ligne)) { /// Lecture de chaque ligne du fichier et la stocker dans la variable ligne
-                    cout << ligne << endl;  /// Affichage de chaque ligne
-                }
-                fichier.close();
-            }
-
-
-
+#endif // message_H_INCLUDED
