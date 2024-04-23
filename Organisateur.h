ca05bb3d60aae459652c219a6c8fd47a0b0c1038
hadilt1 <149814073+hadilt1@users.noreply.github.com>
2024-04-23 21:19:21 +0100

    Update Organisateur.h

--- Organisateur.h
+++ Organisateur.h
@@ -13,9 +13,9 @@ class organisateur : public Personne {
 
     string tache;
 public:
-    organisateur(){};
-    organisateur(string nom,string prenom,string email , string tache):Personne(nom,prenom, email) {this->tache=tache;};
-    ~organisateur(){};
+    organisateur(){}; // constructeur de la classe organisateur
+    organisateur(string nom,string prenom,string email , string tache):Personne(nom,prenom, email) {this->tache=tache;}; // appel au constructeur de la clase mère personne 
+    ~organisateur(){}; //destructeur de la classe organisateur
 
     friend istream& operator>>(istream& is, organisateur& o) {
 
@@ -33,7 +33,7 @@ public:
         return os;
     }
 
-    void modifier() ;
+    void modifier() ; // fonction pour modifier l'un des attributs de la classe organisateur
 };
 
 
