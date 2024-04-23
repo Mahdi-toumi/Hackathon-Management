423bf5ad1dc4bc75e81bca6669acc186922b9c59
hadilt1 <149814073+hadilt1@users.noreply.github.com>
2024-04-23 20:33:18 +0100

    Update Sponsor.h

--- Sponsor.h
+++ Sponsor.h
@@ -9,7 +9,7 @@
 using namespace std;
 
 class sponsor {
-    static int nextID;
+    static int nextID; // pour generer des id consecutifs et uniques
 
     int id;
     string nom;
@@ -19,19 +19,19 @@ class sponsor {
     list<string> produits_sponso;
 
 public:
-    sponsor();
+    sponsor(); // constructeur de la classe sponsor 
     sponsor(string, string, double, float);
-    ~sponsor();
+    ~sponsor(); // destructeur de la classe sponosr
 
-    int get_id_sponosr() { return id; }
-    float getmontant() {return montant_de_sponsoring ;}
-    string getnom() { return nom; }
+    int get_id_sponosr() { return id; } // retourne la valeur de l'id 
+    float getmontant() {return montant_de_sponsoring ;} //retourne la valeur du montant de sponsoring 
+    string getnom() { return nom; } // retourne le'attribut nom 
 
-    void modifier_sponsor();
+    void modifier_sponsor(); // fpnction qui modifie l'un des attributs des sponsors 
 
-    void ajouter_produit() ;
+    void ajouter_produit() ; // fonction qui ajoute des produits a l'attribut produits_sponso
 
-    void spprimer_produit() ;
+    void spprimer_produit() ; // fonction qui supprime des produits de la liste des produits 
 
 
 
